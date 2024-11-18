179ee981165d0f4e09c238f3053981e347457705
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2024-11-18 11:50:02 +0900

    feat: 공통 스타일 추가

---
+++ src/styles/colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+  MainColor: "#F1729B",
+  Pink: "#D04578",
+  Blue: "#7EB6FF", //인증
+  Star: "#FFA31A", //별점
+
+  //gray
+  White: "#FFFFFF",
+  Gray6: "#F2F2F7",
+  Gray5: "#E5E5EA",
+  Gray4: "#D1D1D6",
+  Gray3: "#C7C7CC",
+  Gray2: "#AEAEB2",
+  Gray1: "#8E8E93",
+  Gray: "#E8E8E8",
+  Black: "#1C1C1E",
+};
