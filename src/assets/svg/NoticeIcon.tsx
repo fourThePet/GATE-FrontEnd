fd360bb7f87920a10dffedf3436fda6b81b129d7
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2024-11-26 02:48:57 +0900

    feat: 검색리스트 UI 구현

---
+++ src/assets/svg/NoticeIcon.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgNoticeIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 15 15"
+    {...props}
+  >
+    <path fill="#9A9EA6" d="M7.5 15a7.5 7.5 0 1 0 0-15 7.5 7.5 0 0 0 0 15" />
+    <path
+      fill="#fff"
+      d="M6.755 11.666h1.703v-5.7H6.755zm.863-6.75a.95.95 0 0 0 1.015-.935.94.94 0 0 0-.63-.885.9.9 0 0 0-.383-.047.94.94 0 0 0-1.026.933.953.953 0 0 0 1.026.934z"
+    />
+  </svg>
+);
+export default SvgNoticeIcon;
---
+++ src/pages/place-list/index.styles.ts
@@ -0,0 +1,8 @@
+import { css } from "@emotion/react";
+
+export const noticeStyle = css`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin-left: 8%;
+`;
