b5fccdd4c5dc935d783b126934d6fb548c2c723a
minju0423 <lmj042398@gmail.com>
2024-11-26 10:39:11 +0900

    feat: 회원가입 완료 페이지

---
+++ src/pages/onboarding/completion/index.styles.ts
@@ -0,0 +1,10 @@
+import { css } from '@emotion/react'
+
+export const wrapper = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    gap: 30px;
+    margin: auto auto;
+`
