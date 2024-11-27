0d12c70368cc70256fe2ddf136a2d8afe273eca8
minju0423 <lmj042398@gmail.com>
2024-11-27 11:16:42 +0900

    feat: 카카오 로그인 api 연동

---
+++ src/pages/auth/kakao-callback/index.tsx
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function KakaoCallback() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // URL에서 쿼리 파라미터 추출
+    const queryParams = new URLSearchParams(window.location.search);
+    const accessToken = queryParams.get("accessToken");
+    const refreshToken = queryParams.get("refreshToken");
+    const status = queryParams.get("status");
+
+    if (accessToken && refreshToken && status) {
+      // 토큰 저장
+      localStorage.setItem("accessToken", accessToken);
+      localStorage.setItem("refreshToken", refreshToken);
+
+      // 상태에 따른 페이지 이동
+      if (status === "ACTIVE") {
+        navigate("/"); // 메인 페이지로 이동
+      } else if (status === "NONACTIVE") {
+        navigate("/onboarding/user"); // 회원가입 페이지로 이동
+      } else {
+        console.log("알 수 없는 상태입니다:", status);
+      }
+    } else {
+      console.log("액세스 토큰 또는 리프레시 토큰이 없습니다.");
+      
+    }
+  }, [navigate]);
+
+  return <></>;
+};
+
+
