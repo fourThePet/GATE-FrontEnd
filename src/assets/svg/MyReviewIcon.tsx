4d8ec5f5d49f7e6d4f009fadf7383d4a687a2d06
minju0423 <lmj042398@gmail.com>
2024-11-28 14:29:19 +0900

    feat: 마이페이지에 필요한 svg 추가

---
+++ src/assets/svg/MyReviewIcon.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgMyReviewIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 16 20"
+    {...props}
+  >
+    <path
+      fill="#000"
+      d="M9 12H5a1 1 0 0 0 0 2h4a1 1 0 0 0 0-2m4-10h-1.18A3 3 0 0 0 9 0H7a3 3 0 0 0-2.82 2H3a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V5a3 3 0 0 0-3-3M6 3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v1H6zm8 14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h1v1a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4h1a1 1 0 0 1 1 1zm-3-9H5a1 1 0 0 0 0 2h6a1 1 0 1 0 0-2"
+    />
+  </svg>
+);
+export default SvgMyReviewIcon;
