e28556891e5c332e22d5e1952088ef5900adfe5c
minju0423 <125239229+minju0423@users.noreply.github.com>
2024-11-29 16:48:03 +0900

    [GATE-86] 반려동물 프로필 등록 API 연동 (#43)
    * fix: yarn.lock
    
    * fix: package.json 수정
    
    * fix: react-star-rating-component 설치
    
    * feat: 반려동물 프로필 등록
    
    * fix: 주석제거

--- src/api/api.ts
+++ src/api/api.ts
@@ -19,3 +19,23 @@ api.interceptors.request.use(
       return Promise.reject(error);
     }
 );
+
+export const formAPI = axios.create({
+  baseURL : import.meta.env.VITE_BASE_URL,
+  headers: {
+    'Content-Type': 'multipart/form-data',
+  },
+});
+
+formAPI.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
