909e5c3b1d806b6f1f5cc17fa47365eb7e8d4662
OHRRRRR <3819713@naver.com>
2024-12-02 16:45:32 +0900

    fix: conflict 해결

--- src/api/places/index.ts
+++ src/api/places/index.ts
@@ -136,3 +136,13 @@ export const useGetPlaces = (
 
   return { places, isLoading, error };
 };
+
+export const getPlacesInfo = async (placeId: number) => {
+  try {
+    const response = await api.get(`/places/${placeId}`);
+    return response.data.result; // 필요한 데이터만 반환
+  } catch (error) {
+    console.error("Error fetching place info:", error);
+    throw error;
+  }
+};
