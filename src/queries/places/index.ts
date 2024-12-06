a8f6a284ded046a8b465abf20708c4cf7d70e053
OHRRRRR <3819713@naver.com>
2024-12-06 15:54:54 +0900

    feat: 지역 리스트 조회 api 연동

--- src/queries/places/index.ts
+++ src/queries/places/index.ts
@@ -1,8 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import { QUERY_KEYS } from "../query-keys";
-import { getPlacesInfo, getPlacesCategories, getPlaces_2 } from "../../api";
+import {
+  getPlacesInfo,
+  getPlacesCategories,
+  getPlaces_2,
+  getPlacesCities,
+} from "../../api";
 import { PlacesParam } from "../../interfaces/places";
-
 // 장소 정보 가져오기 훅
 export const useGetPlacesInfo = (placeId: number) => {
   return useQuery({
@@ -20,26 +24,42 @@ export const useGetPlacesInfo = (placeId: number) => {
 
 export const useGetPlacesCategories = () => {
   return useQuery({
-    queryKey : QUERY_KEYS.GET_PLACES_CATEGORIES,
-    queryFn : async() => {
+    queryKey: QUERY_KEYS.GET_PLACES_CATEGORIES,
+    queryFn: async () => {
       try {
         return await getPlacesCategories();
-      }catch{
+      } catch {
         throw new Error("카테고리를 가져오는 데 실패했습니다.");
       }
-    }
-  })
-}
+    },
+  });
+};
 
-export const useGetPlaces2 = (params : PlacesParam) => {
+export const useGetPlaces2 = (params: PlacesParam) => {
   return useQuery({
-    queryKey : QUERY_KEYS.GET_PLACES(params),
-    queryFn : async() => {
-      try{
-        return await getPlaces_2(params)
-      }catch{
+    queryKey: QUERY_KEYS.GET_PLACES(params),
+    queryFn: async () => {
+      try {
+        return await getPlaces_2(params);
+      } catch {
         throw new Error("장소 정보를 가져오는 데 실패했습니다.");
       }
-    }
-  })
-}
+    },
+  });
+};
+
+// 지역 리스트 조회 훅
+export const useGetPlacesCities = () => {
+  return useQuery({
+    queryKey: QUERY_KEYS.GET_PLACES_CITIES, // 캐싱 키
+    queryFn: async () => {
+      try {
+        return await getPlacesCities(); // API 호출
+      } catch (error: any) {
+        console.error("지역 리스트 조회 중 오류 발생:", error.message);
+        throw error; // 에러를 throw하여 React Query가 캐치하도록 함
+      }
+    },
+    staleTime: 1000 * 60 * 5, // 데이터 캐싱 시간 (5분)
+  });
+};
