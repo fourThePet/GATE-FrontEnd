6dfd2f69b4f196c1acae7d7cbcdc99f1b9ef6f5b
OHRRRRR <3819713@naver.com>
2024-12-07 14:33:53 +0900

    feat: 일정 메인페이지 UI 구현중

--- src/pages/plan/components/travel-form/index.tsx
+++ src/pages/plan/components/travel-form/index.tsx
@@ -22,6 +22,7 @@ export const TravelForm = ({
         align-items: center;
         margin-bottom: 10px;
         padding: 10px 0;
+        border-bottom: 1px solid #eaeaea;
       `}
       onClick={onClick}
     >
