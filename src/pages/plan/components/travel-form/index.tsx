ac66b883bf83eb7770834f879f2f3f0a2fbb795b
OHRRRRR <3819713@naver.com>
2024-12-07 15:57:00 +0900

    design: 일정 메인 탭 수정

--- src/pages/plan/components/travel-form/index.tsx
+++ src/pages/plan/components/travel-form/index.tsx
@@ -20,7 +20,6 @@ export const TravelForm = ({
         align-items: center;
         margin-bottom: 10px;
         padding: 10px 0;
-        border-bottom: 1px solid #eaeaea;
       `}
     >
       <img
