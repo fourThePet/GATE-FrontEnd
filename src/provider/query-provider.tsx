1eb70d94602afc2ebfc7487ac024709a1eaa27c0
OHRRRRR <3819713@naver.com>
2024-12-09 01:37:08 +0900

    feat: getPlans, postCreatePlan api 설정

--- src/provider/query-provider.tsx
+++ src/provider/query-provider.tsx
@@ -1,9 +1,11 @@
 import { PropsWithChildren } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+// QueryClient 인스턴스 생성
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
+      retry: 1,
       refetchOnWindowFocus: false,
     },
   },
