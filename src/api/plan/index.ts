dc1a3d108e7ea38a5557c2b6ff543a1d1a25dd96
minju0423 <lmj042398@gmail.com>
2024-12-09 00:33:58 +0900

    chore: 주석 제거

--- src/api/plan/index.ts
+++ src/api/plan/index.ts
@@ -1,6 +1,6 @@
 import { api } from "../api";
 
-// 카테고리 데이터 요청 및 가공
+
 export const getPlanByPlanId = async (planId : number) => {
     try {
         const response = await api.get(`/plan/${planId}`);
