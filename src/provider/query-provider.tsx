2bcea2051df2e5631e4217ffb28c3f02f5adb019
OHRRRRR <3819713@naver.com>
2024-12-09 17:29:46 +0900

    fix: query-provider 복구

--- src/provider/query-provider.tsx
+++ src/provider/query-provider.tsx
@@ -1,23 +1,16 @@
-import React from "react";
+import { PropsWithChildren } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-// QueryClient 인스턴스 생성
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      retry: 1,
       refetchOnWindowFocus: false,
     },
   },
 });
 
-// QueryProvider 컴포넌트 정의 및 default export
-const QueryProvider: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
+export default function QueryProvider({ children }: PropsWithChildren) {
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
-};
-
-export default QueryProvider;
+}
