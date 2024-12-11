8487646c20fd290b551356aa1f78e4c437dcfadf
OHRRRRR <3819713@naver.com>
2024-12-12 00:17:25 +0900

    feat: /place 페이지 검색 API 연동

--- src/pages/place/filter-place/index.tsx
+++ src/pages/place/filter-place/index.tsx
@@ -124,8 +124,18 @@ export default function FilterPlace({ setIsFilterModalOpen }: Props) {
           }),
         ]}
       >
-        <GrayBorderButton title="초기화" width="50%" height="48px" onClick={handleReset}/>
-        <MainPinkButton width="50%" height="48px" onClick={handleApply} >적용하기</MainPinkButton>
+        <GrayBorderButton
+          title="초기화"
+          width="50%"
+          height="48px"
+          onClick={handleReset}
+        />
+        <MainPinkButton
+          title="적용하기"
+          width="50%"
+          height="48px"
+          onClick={handleApply}
+        />
       </div>
     </div>
   );
