324f574796abc1815b89e5155553f6e6a8d0c958
OHRRRRR <3819713@naver.com>
2024-12-14 02:12:18 +0900

    fix: 오류수정2

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  // const env = loadEnv(mode, process.cwd());
   return {
     plugins: [
       react(),
