32921bea37caf192f6d8cad97325c7cd109022ab
OHRRRRR <3819713@naver.com>
2024-12-14 02:07:54 +0900

    feat: 배포 오류 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+  // const env = loadEnv(mode, process.cwd());
   return {
     plugins: [
       react(),
