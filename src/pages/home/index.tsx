d827ef69fb1de2dbefe77623f0b23605eddbd58d
OHRRRRR <3819713@naver.com>
2024-12-16 13:27:01 +0900

    fix: home 코드 복구

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,7 +3,28 @@ import { PageWrapper } from "../../styles/ui";
 import SearchbarCategory from "./components/searchbar-category";
 import TodayBenefit from "./components/today-benefit";
 import Best10 from "./components/Best10";
+import { useEffect, useState } from "react";
+import { LoadingBar, PetRegistrationModal } from "../../components";
+import { useAuthStore } from "../../stores/useAuthStore";
+import { useGetDogsProfiles } from "../../queries";
 export default function Home() {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const { isLoggedIn } = useAuthStore();
+  const { data: dogs, isLoading } = useGetDogsProfiles(); // 데이터 로딩 중 undefined 유지
+  useEffect(() => {
+    if (isLoggedIn && !isLoading) {
+      if (dogs?.length === 0) {
+        setIsModalOpen(true); // 반려동물이 없을 때 모달 열기
+      } else {
+        setIsModalOpen(false); // 반려동물이 있을 때 모달 닫기
+      }
+    } else if (!isLoggedIn) {
+      setIsModalOpen(false); // 로그아웃 상태에서는 모달 닫기
+    }
+  }, [isLoggedIn, dogs, isLoading]);
+  if (isLoading) {
+    return <LoadingBar />;
+  }
   return (
     <>
       <div
@@ -30,6 +51,12 @@ export default function Home() {
         <SearchbarCategory />
         <TodayBenefit />
         <Best10 />
+        {isModalOpen && (
+          <PetRegistrationModal
+            isOpen={isModalOpen}
+            setIsOpen={setIsModalOpen}
+          />
+        )}
       </div>
     </>
   );
