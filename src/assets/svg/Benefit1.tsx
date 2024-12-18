c9c521729f32b9a02a405f39928cf5e8ecba9672
OHRRRRR <63331572+OHRRRRR@users.noreply.github.com>
2024-12-18 16:26:14 +0900

    Fix/map (#194)
    * feat: 주소복사 버튼 구현
    
    * fix: 오류 수정
    
    * fix: 배포오류해결

--- src/assets/svg/Benefit1.tsx
+++ src/assets/svg/Benefit1.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import type { SVGProps } from "react";
 const SvgBenefit1 = (props: SVGProps<SVGSVGElement>) => (
   <svg
--- src/assets/svg/Benefit3.tsx
+++ src/assets/svg/Benefit3.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import type { SVGProps } from "react";
 const SvgBenefit3 = (props: SVGProps<SVGSVGElement>) => (
   <svg
